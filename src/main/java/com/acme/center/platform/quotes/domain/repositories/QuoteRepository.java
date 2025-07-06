16dbc98e80a75d703b2d7101cd6c4e01045057f5
MarianoOblitas <u202310222@upc.edu.pe>
2025-07-06 13:30:17 -0500

    chore: remove unused repositories copy

--- src/main/java/com/acme/center/platform/quotes/domain/repositories/QuoteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.acme.center.platform.quotes.domain.repositories;
-
-import com.acme.center.platform.quotes.domain.model.aggregates.Quote;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuoteRepository extends JpaRepository<Quote, Long> {}
